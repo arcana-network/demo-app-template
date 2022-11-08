d8da21f9bd12462b1e6b38a3bf9e9a0e17acf627
Ajith Ranka <ajithranka@users.noreply.github.com>
2022-11-08 11:40:07 +0000

    All template changes

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,13 +1,15 @@
 // STORAGE-IMPORT : Import Arcana storage standalone
 // ...
 
+const FIX_ME = null;
+
 function createStorageService() {
   let storage;
 
   async function init() {
     if (!storage) {
       // STORAGE-1: Create an instance of Arcana StorageProvider.
-      // storage = ...
+      storage = FIX_ME;
     }
   }
 
